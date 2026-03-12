3947bc2f799d051e9f5eb2efd9e724e4ed72990a
anddyZunigaVega <anddy.zuniga.vega@est.una.ac.cr>
2026-03-11 21:24:35 -0600

    Delete module-info.java
    Eliminar carpeta no funcional

--- src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module cr.ac.una.sistemafichas {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens cr.ac.una.sistemafichas to javafx.fxml;
-    exports cr.ac.una.sistemafichas;
-}
