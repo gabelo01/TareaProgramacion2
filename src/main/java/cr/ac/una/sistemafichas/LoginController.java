ec329ce9d0c0fc2d31199475b44db082fa56545f
anddyZunigaVega <anddy.zuniga.vega@est.una.ac.cr>
2026-03-11 23:07:35 -0600

    Cambio Nombre
    nombre controller

--- src/main/java/cr/ac/una/sistemafichas/LoginController.java
+++ src/main/java/cr/ac/una/sistemafichas/LoginController.java
@@ -2,10 +2,13 @@ package cr.ac.una.sistemafichas;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
 
-public class PrimaryController {
+public class LoginController {
 
     @FXML
+    private AnchorPane root;
+
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
     }
