b86be4d626b6fdf184fa26aca7334445189e048d
anddyZunigaVega <anddy.zuniga.vega@est.una.ac.cr>
2026-03-12 20:50:01 -0600

    LoginView
    se creo  package controller y view

---
+++ src/main/java/cr/ac/una/sistemafichas/controller/LoginController.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package cr.ac.una.sistemafichas.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author agamg
+ */
+public class LoginController implements Initializable {
+
+    @FXML
+    private AnchorPane root;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
