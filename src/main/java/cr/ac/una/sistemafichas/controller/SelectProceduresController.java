facd83ce8a616b044898d67cb6866e390885e3a7
anddyZunigaVega <anddy.zuniga.vega@est.una.ac.cr>
2026-03-12 21:38:08 -0600

    Selector de Tramites

---
+++ src/main/java/cr/ac/una/sistemafichas/controller/SelectProceduresController.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package cr.ac.una.sistemafichas.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+/**
+ * FXML Controller class
+ *
+ * @author agamg
+ */
+public class SelectProceduresController implements Initializable {
+
+    @FXML
+    private Button getTicketButton;
+    @FXML
+    private Button preferentialButton;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
