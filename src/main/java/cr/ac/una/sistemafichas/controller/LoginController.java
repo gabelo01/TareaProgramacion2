e87e561161b611a0f5d38f319acdf07d50902ac8
anddyZunigaVega <anddy.zuniga.vega@est.una.ac.cr>
2026-03-13 11:21:53 -0600

    LoginView
    Prueba de contraseña

--- src/main/java/cr/ac/una/sistemafichas/controller/LoginController.java
+++ src/main/java/cr/ac/una/sistemafichas/controller/LoginController.java
@@ -8,7 +8,13 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.layout.AnchorPane;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -19,13 +25,51 @@ public class LoginController implements Initializable {
 
     @FXML
     private AnchorPane root;
+    @FXML
+    private PasswordField password;
+    @FXML
+    private Button ingresar;
 
     /**
      * Initializes the controller class.
      */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+   @Override
+public void initialize(URL url, ResourceBundle rb) {
+    
+    ingresar.setOnAction(event -> validarID());
+    
+}
+
+@FXML
+private void validarID(){
+
+    String id = password.getText();
+
+    if(id.equals("123")){
+
+        try{
+
+            FXMLLoader loader = new FXMLLoader(
+                getClass().getResource("/cr/ac/una/sistemafichas/view/SelectProcedures.fxml")
+            );
+
+            Parent root = loader.load();
+
+            Stage stage = (Stage) ingresar.getScene().getWindow();
+
+            Scene scene = new Scene(root);
+
+            stage.setScene(scene);
+            stage.show();
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+    }else{
+        System.out.println("ID incorrecto");
+    }
+
+}
     
 }
