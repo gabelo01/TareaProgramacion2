b4ae4a6d5cc8f93b5930e7b28d193ea378518af6
anddyZunigaVega <anddy.zuniga.vega@est.una.ac.cr>
2026-03-13 19:04:49 -0600

    cosas inecesarias

--- src/main/java/cr/ac/una/sistemafichas/controller/PreferentialController.java
+++ src/main/java/cr/ac/una/sistemafichas/controller/PreferentialController.java
@@ -1,22 +1,62 @@
 package cr.ac.una.sistemafichas.controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.stage.Stage;
 
-/**
- * FXML Controller class
- *
- * @author agamg
- */
 public class PreferentialController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private Button getTicket;
+
+    @FXML
+    private Button cancel;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+
+        getTicket.setOnAction(e -> validarPin());
+        cancel.setOnAction(e -> volver());
+
+    }
+
+    private void validarPin() {
+
+        String pin = password.getText();
+
+        if (pin.equals("123")) {
+            System.out.println(1);
+        } else {
+            System.out.println("PIN incorrecto");
+        }
+    }
+
+    private void volver() {
+
+        try {
+            FXMLLoader loader = new FXMLLoader(
+                getClass().getResource("/cr/ac/una/sistemafichas/view/SelectProcedures.fxml")
+            );
+
+            Parent root = loader.load();
+
+            Stage stage = (Stage) cancel.getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/cr/ac/una/sistemafichas/controller/SelectProceduresController.java
+++ src/main/java/cr/ac/una/sistemafichas/controller/SelectProceduresController.java
@@ -1,29 +1,56 @@
 package cr.ac.una.sistemafichas.controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
 
-/**
- * FXML Controller class
- *
- * @author agamg
- */
 public class SelectProceduresController implements Initializable {
 
     @FXML
     private Button getTicketButton;
+
     @FXML
     private Button preferentialButton;
 
-    /**
-     * Initializes the controller class.
-     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+       
+        getTicketButton.setOnAction(e ->getPin());
+        preferentialButton.setOnAction(event -> {
+            try {
+                openPreferential();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+    }
+
+    private void openPreferential() throws IOException {
+
+        FXMLLoader loader = new FXMLLoader(
+                getClass().getResource("/cr/ac/una/sistemafichas/view/Preferential.fxml")
+        );
+
+        Parent root = loader.load();
+
+        Stage stage = (Stage) preferentialButton.getScene().getWindow();
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
+    }
     
+    private void getPin(){
+    
+        System.out.println(2);
+    }
 }
