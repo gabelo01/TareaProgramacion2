fc589a2489d351a2a5dd7ee86794594d41ed45c7
gabelo01 <232612846+gabriel01-cloud@users.noreply.github.com>
2026-03-26 10:39:43 -0600

    mejoras en maintenanceCompanyController

--- src/main/java/cr/ac/una/sistemafichas/controller/admin/MaintenanceCompanyController.java
+++ src/main/java/cr/ac/una/sistemafichas/controller/admin/MaintenanceCompanyController.java
@@ -2,9 +2,24 @@ package cr.ac.una.sistemafichas.controller.admin;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+import java.io.File;
+import javafx.scene.image.Image;
 
 public class MaintenanceCompanyController implements Initializable {
+
+    @FXML
+    private ImageView imgLogo;
+    @FXML
+    private Button btnSelectLogo;
+    @FXML
+    private Button btnSaveCompany;
+    private String logoPath;
 
     /**
      * Initializes the controller class.
@@ -13,5 +28,24 @@ public class MaintenanceCompanyController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
-    
+
+    @FXML
+    private void OnActionBtnSeleccionarLogo(ActionEvent event) {
+    }
+
+    @FXML
+    private void OnActionSaveCompany(ActionEvent event) {
+    }
+    @FXML
+    private void SeleccionarLogo(){
+        FileChooser chooser = new FileChooser();
+        chooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Imagenes","*.png","*.jpg","*.jpeg"));
+        chooser.setTitle("Seleccionar Logo");
+        File file = chooser.showOpenDialog(null);
+        if(file!=null){
+            String route = file.getAbsolutePath();
+            Image image = new Image(file.toURI().toString());
+            imgLogo.setImage(image);
+        }
+    }
 }
