42d1804d9ad9c3c1e6ec80b76c1a765133a973bc
MoisesRamirezCordero <moises.ramirez.cordero@est.una.ac.cr>
2026-04-06 19:01:22 -0600

    Updates and Changes to the Administrator Screen

--- src/main/java/cr/ac/una/sistemafichas/controller/admin/SelectMaintenanceController.java
+++ src/main/java/cr/ac/una/sistemafichas/controller/admin/SelectMaintenanceController.java
@@ -14,17 +14,10 @@ import javafx.event.ActionEvent;
 
 public class SelectMaintenanceController extends Controller {
 
-    @FXML private MFXButton btnIndicators;
-    @FXML private MFXButton btnClients;
-    @FXML private MFXButton btnConfig;
-    @FXML private MFXButton btnVolver;
-    @FXML private Label lbCompany;
-    @FXML private ImageView imgLogo;
-
+    private Label lbCompany;
+    private ImageView imgLogo;
     private static final String CONFIG_PATH = "data/config.json";
-    
-    @FXML
-    private MFXButton btnBranchesAndProcedures;
+
     
     @Override
     public void initialize() {
@@ -32,28 +25,25 @@ public class SelectMaintenanceController extends Controller {
     }
     
     @FXML
-    private void btnVolver() {
-        FlowController.getInstance().goViewReplace("admin/LoginAdminView");
+    private void onActionBtnVolver() {
+        FlowController.getInstance().goView("admin/LoginAdminView");
     }
 
     @FXML
-    private void btnBranchesAndProcedures(ActionEvent event) {
-        FlowController.getInstance().goViewReplace("admin/ProceduresAndBranchMaintenanceView");
+    private void onActionBtnProcedures(ActionEvent event) {
+        FlowController.getInstance().goView("admin/ProceduresAndBranchMaintenanceView");
     }
     
-    @FXML
-    private void btnIndicators() {
-        FlowController.getInstance().goViewReplace("Indicators");
+    private void onActionBtnIndicators() {
+        FlowController.getInstance().goView("Indicators");
     }
 
-    @FXML
-    private void btnClients() {
-        FlowController.getInstance().goViewReplace("Clients");
+    private void onActionBtnClients() {
+        FlowController.getInstance().goView("Clients");
     }
 
-    @FXML
-    private void btnConfig() {
-        FlowController.getInstance().goViewReplace("admin/ConfigView");
+    private void onActionBtnConfig() {
+        FlowController.getInstance().goView("admin/ConfigView");
     }
 
     private void cargarEncabezado() {
@@ -73,5 +63,23 @@ public class SelectMaintenanceController extends Controller {
             System.err.println("No se pudo cargar el logo.");
         }
     }
+
+
+    @FXML
+    private void onActionBtnBranches(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionBtnIndicators(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionBtnClients(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionBtnConfig(ActionEvent event) {
+    }
+
 
 }
