d784e6a908c041c03931dd5e9ec8c19f8cfff52b
anddyZunigaVega <anddy.zuniga.vega@est.una.ac.cr>
2026-04-09 13:52:18 -0600

    StationView and Station controller
    Se ajustaron estas partes para mejor flow de las demas view de employee, se abren en nuevas ventanas y no en la mainStage().
    
    se cambio el codigo de FlowController goViewInWindow para que el logo se cargara desde el path de el config.json

--- src/main/java/cr/ac/una/sistemafichas/controller/employee/StationController.java
+++ src/main/java/cr/ac/una/sistemafichas/controller/employee/StationController.java
@@ -42,7 +42,7 @@ public class StationController extends Controller {
         loadHeader();
         loadStation();
         startClock();
-        startAutoRefresh(); // para actualizar tickets y leer json
+        startAutoRefresh(); // para actualizar tickets y que se muestren los cambios cada 5s y leer json
         clearCurrentTicket();
         updateWaitingCount();
 
@@ -175,13 +175,14 @@ public class StationController extends Controller {
         FlowController.getInstance().goViewReplace("employee/WaitListView");
     }
 
+    @FXML
     private void onActionBtnIndicators() {
-        FlowController.getInstance().goViewReplace("employee/IndicatorsView");
+       FlowController.getInstance().goViewInWindow("employee/IndicatorsView");
     }
 
     @FXML
     private void onActionBtnClient() {
-        FlowController.getInstance().goViewReplace("employee/CurrentClientView");
+        FlowController.getInstance().goViewInWindow("employee/CurrentClientView");
     }
     
     @FXML
@@ -217,12 +218,12 @@ public class StationController extends Controller {
 
     @FXML
     private void onActionBtnRegisterClient(ActionEvent event) {
-        FlowController.getInstance().goViewReplace("admin/MaintenanceClientView");
+        FlowController.getInstance().goViewInWindow("admin/MaintenanceClientView");
     }
 
     @FXML
     private void onActionBtnExit(ActionEvent event) {
-        FlowController.getInstance().goViewReplace("employee/StationLogin");
+        FlowController.getInstance().salir();
     }
     
 }
