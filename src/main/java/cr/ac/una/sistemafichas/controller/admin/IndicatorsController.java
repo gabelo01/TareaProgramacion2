fcf34e0e73ea6b81016604e9a75e3abb175f02c1
MoisesRamirezCordero <moises.ramirez.cordero@est.una.ac.cr>
2026-04-14 00:11:56 -0600

    The indicators.fxml file and its controller have been added, all the logic is missing.

---
+++ src/main/java/cr/ac/una/sistemafichas/controller/admin/IndicatorsController.java
@@ -0,0 +1,23 @@
+package cr.ac.una.sistemafichas.controller.admin;
+
+import cr.ac.una.sistemafichas.controller.Controller;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Moises Ramirez
+ */
+public class IndicatorsController extends Controller {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize() {
+
+    }    
+    
+}
--- src/main/java/cr/ac/una/sistemafichas/controller/admin/SelectMaintenanceController.java
+++ src/main/java/cr/ac/una/sistemafichas/controller/admin/SelectMaintenanceController.java
@@ -37,7 +37,7 @@ public class SelectMaintenanceController extends Controller {
 
     @FXML
     private void onActionBtnIndicators(ActionEvent event) {
-        FlowController.getInstance().goView("Indicators");
+        FlowController.getInstance().goView("admin/Indicators");
     }
 
     @FXML
